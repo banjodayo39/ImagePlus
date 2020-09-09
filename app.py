5d183329aeeec003afd89ec8be087e5c2d509f5a
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-09-10 00:50:10 +0100

    Implemented New UI + Updated requirements.txt

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/resize-img')
+def resizeimg():
+    return render_template('./utilities/resizeimage.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
